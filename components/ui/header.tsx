67bb90bf7ef7d7cfd3fc4d8664704c346e807844
Cláudio Barreira <claudio.barreira89@gmail.com>
2024-10-04 00:03:09 +0100

    fix: header mobile

--- components/ui/header.tsx
+++ components/ui/header.tsx
@@ -2,17 +2,18 @@
 
 import Link from "next/link";
 import Logo from "./logo";
+import Image from "next/image";
 
 export default function Header() {
   return (
     <header className="z-30 mt-2 w-full md:mt-5 fixed md:top-5 top-2">
       <div className="mx-auto max-w-6xl px-4 sm:px-6">
-        <div className="relative flex h-20 items-center backdrop-blur-xl justify-between gap-3 rounded-full  px-6 before:pointer-events-none before:absolute before:inset-0 before:rounded-[inherit] before:border before:border-transparent before:[background:linear-gradient(to_right,theme(colors.gray.800),theme(colors.gray.700),theme(colors.gray.800))_border-box] before:[mask-composite:exclude_!important] before:[mask:linear-gradient(white_0_0)_padding-box,_linear-gradient(white_0_0)] after:absolute after:inset-0 after:-z-10 after:backdrop-blur-sm">
+        <div className="relative flex sm:h-20 py-4 sm:py-0 items-center backdrop-blur-xl justify-between gap-3 rounded-full  px-6 before:pointer-events-none before:absolute before:inset-0 before:rounded-[inherit] before:border before:border-transparent before:[background:linear-gradient(to_right,theme(colors.gray.800),theme(colors.gray.700),theme(colors.gray.800))_border-box] before:[mask-composite:exclude_!important] before:[mask:linear-gradient(white_0_0)_padding-box,_linear-gradient(white_0_0)] after:absolute after:inset-0 after:-z-10 after:backdrop-blur-sm">
           <div className="flex flex-1 items-center">
             <Logo />
           </div>
 
-          <ul className="flex flex-1 items-center justify-end gap-4">
+          <ul className="flex sm:flex-1 items-center justify-end gap-4">
             <li>
               <Link
                 href="https://docs.lendfinity.xyz/lendfinity-docs"
@@ -33,7 +34,15 @@ export default function Header() {
               <Link
                 href="https://app.lendfinity.xyz"
                 target="_blank"
-                className="rounded-full py-3 px-4 text-lg btn-sm bg-gradient-to-t from-purple-600 to-purple-500 bg-[length:100%_100%] bg-[bottom] text-white shadow-[inset_0px_1px_0px_0px_theme(colors.white/.16)] hover:bg-[length:100%_150%]"
+                className="sm:block hidden rounded-full py-3 px-4 text-lg btn-sm bg-gradient-to-t from-purple-600 to-purple-500 bg-[length:100%_100%] bg-[bottom] text-white shadow-[inset_0px_1px_0px_0px_theme(colors.white/.16)] hover:bg-[length:100%_150%]"
+              >
+                Launch App
+              </Link>
+
+              <Link
+                href="https://github.com/LendFinity"
+                target="_blank"
+                className="block sm:hidden text-purple-400"
               >
                 Launch App
               </Link>
--- components/ui/logo.tsx
+++ components/ui/logo.tsx
@@ -1,11 +1,13 @@
 import Link from "next/link";
 import Image from "next/image";
 import logo from "@/public/images/logo.svg";
+import logoMobile from "@/public/images/lendfinity-mobile.svg";
 
 export default function Logo() {
   return (
-    <Link href="/" className="inline-flex shrink-0" aria-label="Cruip">
-      <Image src={logo} alt="Cruip Logo" width={180} height={64} />
+    <Link href="/" className="inline-flex shrink-0">
+      <Image src={logo} alt="Lendfinity Logo" width={180} height={64} className="sm:block hidden" />
+      <Image src={logoMobile} alt="Lendfinity Logo" width={60} height={40} className="block sm:hidden" />
     </Link>
   );
 }
