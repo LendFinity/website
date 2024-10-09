85846e733a9d66ce5bf49bbdaf7d9c57a1d46f27
Cláudio Barreira <claudio.barreira89@gmail.com>
2024-10-09 23:23:59 +0100

    feat: add waitlist form

--- app/(default)/page.tsx
+++ app/(default)/page.tsx
@@ -19,7 +19,7 @@ export default function Home() {
       <Metrics />
       <Workflows />
       <Testimonials />
-      {/* <Cta /> */}
+      <Cta />
     </>
   );
 }
--- components/cta.tsx
+++ components/cta.tsx
@@ -1,5 +1,6 @@
 import Image from "next/image";
 import BlurredShape from "@/public/images/blurred-shape.svg";
+import Link from "next/link";
 
 export default function Cta() {
   return (
@@ -20,33 +21,28 @@ export default function Cta() {
         <div className="bg-gradient-to-r from-transparent via-gray-800/50 py-12 md:py-20">
           <div className="mx-auto max-w-3xl text-center">
             <h2
-              className="animate-[gradient_6s_linear_infinite] bg-[linear-gradient(to_right,theme(colors.gray.200),theme(colors.indigo.200),theme(colors.gray.50),theme(colors.indigo.300),theme(colors.gray.200))] bg-[length:200%_auto] bg-clip-text pb-8 font-nacelle text-3xl font-semibold text-transparent md:text-4xl"
+              className="animate-[gradient_6s_linear_infinite] bg-[linear-gradient(to_right,theme(colors.gray.200),theme(colors.indigo.200),theme(colors.gray.50),theme(colors.indigo.300),theme(colors.gray.200))] bg-[length:200%_auto] bg-clip-text pb-4 font-nacelle text-3xl font-semibold text-transparent md:text-5xl"
               data-aos="fade-up"
             >
-              Join the content-first platform
+              Unlock the Future<br />
+              of DeFi Lending.
             </h2>
+            <div className="pb-4">
+              Join the waitlist to get early access to our innovative lending platform <br />
+              and stay ahead in the world of decentralized finance.
+            </div>
+
             <div className="mx-auto max-w-xs sm:flex sm:max-w-none sm:justify-center">
-              <div data-aos="fade-up" data-aos-delay={400}>
-                <a
-                  className="btn group mb-4 w-full bg-gradient-to-t from-indigo-600 to-indigo-500 bg-[length:100%_100%] bg-[bottom] text-white shadow-[inset_0px_1px_0px_0px_theme(colors.white/.16)] hover:bg-[length:100%_150%] sm:mb-0 sm:w-auto"
-                  href="#0"
-                >
-                  <span className="relative inline-flex items-center">
-                    Start Building
-                    <span className="ml-1 tracking-normal text-white/50 transition-transform group-hover:translate-x-0.5">
-                      -&gt;
-                    </span>
-                  </span>
-                </a>
-              </div>
-              <div data-aos="fade-up" data-aos-delay={600}>
-                <a
-                  className="btn relative w-full bg-gradient-to-b from-gray-800 to-gray-800/60 bg-[length:100%_100%] bg-[bottom] text-gray-300 before:pointer-events-none before:absolute before:inset-0 before:rounded-[inherit] before:border before:border-transparent before:[background:linear-gradient(to_right,theme(colors.gray.800),theme(colors.gray.700),theme(colors.gray.800))_border-box] before:[mask-composite:exclude_!important] before:[mask:linear-gradient(white_0_0)_padding-box,_linear-gradient(white_0_0)] hover:bg-[length:100%_150%] sm:ml-4 sm:w-auto"
-                  href="#0"
-                >
-                  Schedule Demo
-                </a>
-              </div>
+
+              <Link
+                href="https://tally.so/r/mOJbM7"
+                target="_blank"
+                className="mt-4 rounded-full py-3 px-8 text-lg btn-sm bg-gradient-to-t from-indigo-600 to-indigo-500 text-white"
+              >
+                Join waitlist
+              </Link>
+
+
             </div>
           </div>
         </div>
